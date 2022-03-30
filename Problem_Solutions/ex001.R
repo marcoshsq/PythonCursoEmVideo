7cda112fba59480bc3acc4fb0be67b1c26738e55
Marcos Henrique <marcoshsq@gmail.com>
2022-03-30 03:50:38 -0300

    Multiples of 3 or 5 - solved with R

---
+++ Problem_Solutions/ex001.R
@@ -0,0 +1,12 @@
+ # Multiples of 3 and 5
+  
+## If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+## Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+  answer <- 0
+  for (i in 1:999) {
+      if (i %% 3 == 0 | i %% 5 == 0) 
+          answer <- answer + i
+  }
+  print(answer)
