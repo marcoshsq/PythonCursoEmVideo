a686624815e6b66cd04fdf16b8b9a00d799d19b7
Marcos Henrique <marcoshsq@gmail.com>
2023-08-01 14:12:45 -0300

    Update EX_003.py

--- Mundo_01/02_Primeiros_passos/EX_003.py
+++ Mundo_01/02_Primeiros_passos/EX_003.py
@@ -7,3 +7,13 @@ print("A soma é: %d!" % (num1 + num2))
 print("A soma é: {}!".format(num1 + num2))
 
 # Os três formatos de print aceitam operações entre variáveis
+
+# Solicitar ao usuário para inserir os números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Calcular a soma dos números
+soma = num1 + num2
+
+# Mostrar o resultado
+print(f"A soma de {num1} e {num2} é igual a {soma}")
